d8a3a4bb74c95dadd10c429fab46332b08048b62
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2024-01-15 13:43:33 +0700

    Create dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
+"use client";
 import Auth from "./auth/page";
+import { getToken } from "@/utils/token";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const storedToken = getToken();
+
+  useEffect(() => {
+    if (!storedToken) {
+      router.push("/auth");
+    }
+  }, [storedToken, router]);
+
   return (
     <main className="flex items-center justify-center">
       <h1>Halo dunia</h1>
